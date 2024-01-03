a10534a6b64539abc29e30c02e6ce3fb2c9c4c09
tolerevo <tolerevo@hotmail.com>
2024-01-03 00:46:45 -0600

    Hook up Search Bar to Spotify Search

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -19,6 +19,9 @@ class App extends React.Component {
     }
     this.addTrack = this.addTrack.bind(this);
     this.removeTrack = this.removeTrack.bind(this);
+    this.updatePlaylistName = this.updatePlaylistName.bind(this);
+    this.savePlaylist = this.savePlaylist.bind(this);
+    this.search = this.search.bind(this);
   }
 
   addTrack(track) {
@@ -38,16 +41,30 @@ class App extends React.Component {
     this.setState({ playlistTracks: tracks });
   }
 
+  updatePlaylistName(name) {
+    this.setState({ playlistName: name })
+  }
+
+  savePlaylist() {
+    alert("this method is linked to the button correctly");
+    // eslint-disable-next-line
+    const trackURIs = this.state.playlistTracks.map(track => track.uri);
+  }
+
+  search(term) {
+    console.log(term);
+  }
+
   render() {
     return (
       <div>
         <h1>Ja<span className="highlight">mmm</span>ing</h1>
         <div className="App">
-          <SearchBar />
+          <SearchBar onSearch={this.search} />
           <div className="App-playlist">
             <SearchResults searchResults={this.state.searchResults} onAdd={this.addTrack} />
             <Playlist playlistName={this.state.playlistName}
-              playlistTracks={this.state.playlistTracks} onRemove={this.removeTrack} />
+              playlistTracks={this.state.playlistTracks} onRemove={this.removeTrack} onNameChange={this.updatePlaylistName} onSave={this.savePlaylist} />
           </div>
         </div>
       </div>
